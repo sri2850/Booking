0973b41a30771568011db9e90fb256e8e9124e1c
sri1056249 <58655583+sri1056249@users.noreply.github.com>
2020-02-12 16:08:35 +0530

    Update local_exec.tf

--- terraform/03-Ansible-Deploy/local_exec.tf
+++ terraform/03-Ansible-Deploy/local_exec.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "backend" {
-  ami                    = "ami-02d0ea44ae3fe9561"
+  ami                    = "ami-0a590332f9f499197"
   instance_type          = "t2.micro"
   key_name               = "${var.key_name}"
   vpc_security_group_ids = ["${var.sg-id}"]
